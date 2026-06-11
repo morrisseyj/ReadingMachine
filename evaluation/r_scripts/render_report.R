1fb7af98fcf2354a5d2476af18af7eb41ec0476e
James Morrissey <morrissey.james1@gmail.com>
2026-06-11 18:00:41 -0400

    trying to fix paths for md render

--- evaluation/r_scripts/render_report.R
+++ evaluation/r_scripts/render_report.R
@@ -1,15 +1,19 @@
 library(rmarkdown)
 
+# Render HTML
 rmarkdown::render(
   input = "evaluation/r_scripts/public_eval_render.Rmd",
   output_format = "html_document",
   output_file = "public_eval.html",
-  output_dir = "evaluation/eval_output"
+  output_dir = "evaluation/eval_output",
+  knit_root_dir = "evaluation/r_scripts"
 )
 
+# Render GitHub Markdown
 rmarkdown::render(
   input = "evaluation/r_scripts/public_eval_render.Rmd",
   output_format = "github_document",
   output_file = "public_eval.md",
-  output_dir = "evaluation/eval_output"
+  output_dir = "evaluation/eval_output",
+  knit_root_dir = "evaluation/r_scripts"
 )
